ddb9e4372852d41a809712b7ef21a0867db39a57
jsomarriba81 <jsomarriba3481@gmail.com>
2019-05-18 13:30:43 +0000

    fixed issues

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -30,7 +30,7 @@ export class Home extends React.Component {
 											alt="..."
 										/>
 										<div className="card-body">
-											<h5 className="card-title">Customized workout plans</h5>
+											<h5 className="card-title">Customized workout plan</h5>
 											<p className="card-text">
 												This is a wider card with supporting text below as a natural lead-in to
 												additional content. This content is a little bit longer.
