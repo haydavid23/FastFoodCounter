91aaaa8441a53825f813575f54dfec994b10bcd9
haydavid23 <hay.david@hotmail.com>
2019-05-31 17:07:05 +0000

    update 2

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -65,6 +65,21 @@ const getState = ({ getStore, setStore }) => {
 				store.query = select;
 				setStore({ store: store });
 				console.log(store.query);
+				fetch("https://trackapi.nutritionix.com/v2/search/instant?query=" + store.query, {
+					headers: {
+						"x-app-key": "2865a994886d0e258357d55037e33f3b",
+						"x-remote-user-id": "0",
+						"x-app-id": "da0a3819"
+					}
+				})
+					.then(response => response.json())
+					.then(data => {
+						store.food = data.common.map((item, index) => {
+							return item.food_name;
+						});
+						setStore({ store: store });
+						console.log(store.food);
+					});
 			}
 		}
 	};
